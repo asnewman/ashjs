5e27ea1fa2b6b1e13560ae340c9f38c9f0548698
asnewman <ashznewman@gmail.com>
2023-10-18 21:43:48 -0700

    Move away from blocks

--- modules/ash.js
+++ modules/ash.js
@@ -12,86 +12,71 @@ export class Ash {
     this.data = data;
   }
 
-  setData(data) {
-    this.data = data;
-
-    return this;
-  }
-
   render() {
     document.getElementById("ashjs").innerHTML = "";
 
-    this.data.forEach((item) => {
-      let newElement = this.generateNewElement(item);
-
-      if (newElement) {
-        document.getElementById("ashjs").appendChild(newElement);
+    this.data.forEach((element) => {
+      const node = this.createNode(element);
+      if (node) {
+        document.getElementById("ashjs").appendChild(node);
       }
     });
   }
 
-  update(replacements) {
-    const newData = this.data.map((item) => {
-      if (item.id && replacements[item.id]) {
-        const newItem = {
-          ...item,
-          ...replacements[item.id],
-        };
-
-        const newElement = this.generateNewElement(newItem);
-        const existingElement = document.getElementById(item.id);
-
-        if (!newElement) {
-          console.warn("Something went wrong generating new element.");
-        } else if (!existingElement) {
-          console.warn("No element found with id: ", item.id);
-        } else {
-          existingElement.replaceWith(newElement);
-        }
-
-        return newItem;
-      } else return item;
-    });
-
-    this.data = newData;
-  }
-
-  generateNewElement(item) {
-    let newElement = document.createElement(typeToHtmlContainer[item.type]);
-    switch (item.type) {
-      case "title":
-        const title = document.createElement("h1");
-        title.innerText = item.content;
-
-        newElement.appendChild(title);
-        if (item.subtitle) {
-          const subtitle = document.createElement("p");
-          subtitle.style.marginTop = "-16px";
-          subtitle.innerText = item.subtitle;
-          newElement.appendChild(subtitle);
-        }
-
-        break;
-      case "text":
-        newElement.innerText = item.content;
-        break;
-      case "list":
-        newElement.id = item.id;
-
-        item.items.forEach((listItem) => {
-          const listItemElement = document.createElement("li");
-          listItemElement.innerText = listItem;
-          newElement.appendChild(listItemElement);
-        });
-        break;
-      case "button":
-        newElement.innerText = item.content;
-        newElement.onclick = item.onclick;
-        break;
-      default:
-        break;
+  createNode(element) {
+    if (typeof element !== "object" || element === null) {
+      return null;
     }
 
-    return newElement;
+    // Get the tag name and its content or children
+    const [tagName, contentOrChildren] = Object.entries(element)[0];
+
+    // Create the DOM element for the tag
+    const node = document.createElement(tagName);
+
+    if (typeof contentOrChildren === "string") {
+      node.textContent = contentOrChildren;
+    } else if (Array.isArray(contentOrChildren)) {
+      contentOrChildren.forEach((childElement) => {
+        const childNode = this.createNode(childElement);
+        if (childNode) {
+          node.appendChild(childNode);
+        }
+      });
+    }
+
+    // Handle additional props (attributes or event listeners)
+    for (const [key, value] of Object.entries(element)) {
+      if (key !== tagName) {
+        if (key.startsWith("on") && typeof value === "function") {
+          node.addEventListener(key.substring(2).toLowerCase(), value);
+        } else {
+          node.setAttribute(key, value.toString());
+        }
+      }
+    }
+
+    return node;
+  }
+
+  update(replacements) {
+    for (const [id, updateProps] of Object.entries(replacements)) {
+      this.updateNodeData(this.data, id, updateProps);
+    }
+    // Re-render the view after updating data
+    this.render();
+  }
+
+  updateNodeData(data, id, updateProps) {
+    for (const item of data) {
+      for (const [tagName, contentOrChildren] of Object.entries(item)) {
+        if (item.id === id) {
+          Object.assign(item, updateProps);
+        }
+        if (Array.isArray(contentOrChildren)) {
+          this.updateNodeData(contentOrChildren, id, updateProps);
+        }
+      }
+    }
   }
 }
--- script.js
+++ script.js
@@ -2,38 +2,130 @@ import { Ash } from "./modules/ash.js";
 
 const ash = new Ash();
 
-const items = [];
-
-const addNewListItem = () => {
-  items.push(new Date());
-
-  ash.update({
-    "log-area": {
-      items,
-    },
-  });
-};
-
-const data = [
+const launches = [
   {
-    type: "title",
-    content: "Dailies",
-    subtitle: "Track your daily challenges.",
+    title: "My todo list",
+    description: "A very simple todo list to do very simple things",
   },
   {
-    type: "text",
-    content: "Let's get started",
+    title: "PhotoMagic",
+    description: "A next-gen photo editing tool with AI enhancements",
   },
   {
-    type: "button",
-    content: "New log",
-    onclick: addNewListItem,
+    title: "TravelMate",
+    description:
+      "Your one-stop solution for all travel bookings and itineraries",
   },
   {
-    type: "list",
-    id: "log-area",
-    items: items,
+    title: "Foodie's Delight",
+    description: "Discover and order delicacies from around the world",
+  },
+  {
+    title: "FitTrack",
+    description: "Track and analyze your fitness journey effectively",
+  },
+  {
+    title: "EcoDrive",
+    description: "An app to monitor and reduce your carbon footprint",
+  },
+  {
+    title: "SpaceExplorer",
+    description: "Explore the universe from your home with our AR tool",
+  },
+  {
+    title: "ShopEase",
+    description: "Experience the future of shopping with AR and VR",
+  },
+  {
+    title: "MusicMingle",
+    description:
+      "Discover, share, and collaborate on music with artists globally",
+  },
+  {
+    title: "News 360",
+    description: "Get 360° perspective on global news without bias",
+  },
+  {
+    title: "RecipeGuru",
+    description: "Find, save, and share world-class recipes with ease",
+  },
+  {
+    title: "GameRealm",
+    description:
+      "Dive deep into the world of gaming with top-tier recommendations",
+  },
+  {
+    title: "EduConnect",
+    description: "Collaborative learning made fun and efficient",
+  },
+  {
+    title: "PetPal",
+    description: "Connect, adopt, and care for pets in your vicinity",
+  },
+  {
+    title: "HealthMonitor",
+    description:
+      "Keep an eye on your health metrics and consult doctors online",
+  },
+  {
+    title: "MovieMood",
+    description:
+      "Watch movies based on your mood and get personalized suggestions",
+  },
+  {
+    title: "StyleSpot",
+    description: "Stay ahead in fashion with AI-powered style recommendations",
+  },
+  {
+    title: "SafeSpace",
+    description: "An app dedicated to mental health and well-being",
+  },
+  {
+    title: "GigFinder",
+    description: "Find freelance gigs in your domain and grow your network",
+  },
+  {
+    title: "GreenGrow",
+    description: "Learn and implement sustainable farming techniques at home",
   },
 ];
 
-ash.setData(data).render();
+function generateLaunchObject(launch, index) {
+  return {
+    div: [
+      {
+        h3: launch.title,
+        id: `launch-${index + 1}`,
+      },
+      {
+        p: launch.description,
+      },
+      {
+        button: "Like",
+        onclick: function () {
+          ash.update({
+            [`launch-${index + 1}`]: {
+              style: "color: green;",
+            },
+          });
+        },
+      },
+    ],
+  };
+}
+
+const data = [
+  {
+    h1: "MiniLaunch",
+  },
+  {
+    p: "Share your project",
+  },
+  {
+    h2: "Recent launches",
+  },
+  ...launches.map(generateLaunchObject),
+];
+
+ash.data = data;
+ash.render();

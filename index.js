a31a587ef3c6052cb1b259f8ba3b161c26ca92bb
asnewman <ashznewman@gmail.com>
2023-10-25 21:14:09 -0700

    the frontend is the frontend, the backend is the backend

--- index.js
+++ index.js
@@ -1,10 +1,98 @@
-const server = Bun.serve({
-  port: 3000,
-  fetch(req) {
-    const url = new URL(req.url);
+import { Hono } from "hono";
+import { cors } from "hono/cors";
 
-    return new Response(generate(), { headers: {} });
+const launches = [
+  {
+    title: "My todo list",
+    description: "A very simple todo list to do very simple things",
   },
-});
+  {
+    title: "PhotoMagic",
+    description: "A next-gen photo editing tool with AI enhancements",
+  },
+  {
+    title: "TravelMate",
+    description:
+      "Your one-stop solution for all travel bookings and itineraries",
+  },
+  {
+    title: "Foodie's Delight",
+    description: "Discover and order delicacies from around the world",
+  },
+  {
+    title: "FitTrack",
+    description: "Track and analyze your fitness journey effectively",
+  },
+  {
+    title: "EcoDrive",
+    description: "An app to monitor and reduce your carbon footprint",
+  },
+  {
+    title: "SpaceExplorer",
+    description: "Explore the universe from your home with our AR tool",
+  },
+  {
+    title: "ShopEase",
+    description: "Experience the future of shopping with AR and VR",
+  },
+  {
+    title: "MusicMingle",
+    description:
+      "Discover, share, and collaborate on music with artists globally",
+  },
+  {
+    title: "News 360",
+    description: "Get 360° perspective on global news without bias",
+  },
+  {
+    title: "RecipeGuru",
+    description: "Find, save, and share world-class recipes with ease",
+  },
+  {
+    title: "GameRealm",
+    description:
+      "Dive deep into the world of gaming with top-tier recommendations",
+  },
+  {
+    title: "EduConnect",
+    description: "Collaborative learning made fun and efficient",
+  },
+  {
+    title: "PetPal",
+    description: "Connect, adopt, and care for pets in your vicinity",
+  },
+  {
+    title: "HealthMonitor",
+    description:
+      "Keep an eye on your health metrics and consult doctors online",
+  },
+  {
+    title: "MovieMood",
+    description:
+      "Watch movies based on your mood and get personalized suggestions",
+  },
+  {
+    title: "StyleSpot",
+    description: "Stay ahead in fashion with AI-powered style recommendations",
+  },
+  {
+    title: "SafeSpace",
+    description: "An app dedicated to mental health and well-being",
+  },
+  {
+    title: "GigFinder",
+    description: "Find freelance gigs in your domain and grow your network",
+  },
+  {
+    title: "GreenGrow",
+    description: "Learn and implement sustainable farming techniques at home",
+  },
+];
 
-console.log(`Listening on http://localhost:${server.port} ...`);
+const app = new Hono();
+
+app.use("/api/*", cors());
+
+app.get("/api/launches", (c) => c.json(launches));
+
+export default app;
--- script.js
+++ script.js
@@ -2,94 +2,6 @@ import { Ash } from "./modules/ash.js";
 
 const ash = new Ash();
 
-const launches = [
-  {
-    title: "My todo list",
-    description: "A very simple todo list to do very simple things",
-  },
-  {
-    title: "PhotoMagic",
-    description: "A next-gen photo editing tool with AI enhancements",
-  },
-  {
-    title: "TravelMate",
-    description:
-      "Your one-stop solution for all travel bookings and itineraries",
-  },
-  {
-    title: "Foodie's Delight",
-    description: "Discover and order delicacies from around the world",
-  },
-  {
-    title: "FitTrack",
-    description: "Track and analyze your fitness journey effectively",
-  },
-  {
-    title: "EcoDrive",
-    description: "An app to monitor and reduce your carbon footprint",
-  },
-  {
-    title: "SpaceExplorer",
-    description: "Explore the universe from your home with our AR tool",
-  },
-  {
-    title: "ShopEase",
-    description: "Experience the future of shopping with AR and VR",
-  },
-  {
-    title: "MusicMingle",
-    description:
-      "Discover, share, and collaborate on music with artists globally",
-  },
-  {
-    title: "News 360",
-    description: "Get 360° perspective on global news without bias",
-  },
-  {
-    title: "RecipeGuru",
-    description: "Find, save, and share world-class recipes with ease",
-  },
-  {
-    title: "GameRealm",
-    description:
-      "Dive deep into the world of gaming with top-tier recommendations",
-  },
-  {
-    title: "EduConnect",
-    description: "Collaborative learning made fun and efficient",
-  },
-  {
-    title: "PetPal",
-    description: "Connect, adopt, and care for pets in your vicinity",
-  },
-  {
-    title: "HealthMonitor",
-    description:
-      "Keep an eye on your health metrics and consult doctors online",
-  },
-  {
-    title: "MovieMood",
-    description:
-      "Watch movies based on your mood and get personalized suggestions",
-  },
-  {
-    title: "StyleSpot",
-    description: "Stay ahead in fashion with AI-powered style recommendations",
-  },
-  {
-    title: "SafeSpace",
-    description: "An app dedicated to mental health and well-being",
-  },
-  {
-    title: "GigFinder",
-    description: "Find freelance gigs in your domain and grow your network",
-  },
-  {
-    title: "GreenGrow",
-    description: "Learn and implement sustainable farming techniques at home",
-  },
-];
-
 function generateLaunchObject(launch, index) {
   return {
     div: [
@@ -114,18 +26,22 @@ function generateLaunchObject(launch, index) {
   };
 }
 
-const data = [
-  {
-    h1: "MiniLaunch",
-  },
-  {
-    p: "Share your project",
-  },
-  {
-    h2: "Recent launches",
-  },
-  ...launches.map(generateLaunchObject),
-];
+fetch("http://localhost:3000/api/launches").then((res) => {
+  res.json().then((launchData) => {
+    const data = [
+      {
+        h1: "MiniLaunch",
+      },
+      {
+        p: "Share your project",
+      },
+      {
+        h2: "Recent launches",
+      },
+      ...launchData.map(generateLaunchObject),
+    ];
 
-ash.data = data;
-ash.render();
+    ash.data = data;
+    ash.render();
+  });
+});
